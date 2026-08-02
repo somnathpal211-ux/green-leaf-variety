a21cc96ca39458646ae4474bc5aa541abf200e41
somnathpal211-ux <somnathpal211@gmail.com>
2026-08-03 03:05:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Green Leaf Variety Loaded Successfully!");
 
+  const cards = document.querySelectorAll(".card, .product");
+
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      alert("ধন্যবাদ! বিস্তারিত তথ্য খুব শীঘ্রই যোগ করা হবে।");
+    });
+  });
+});
