e4c21738fb4208928d76dda8300b0af01710bbfc
somnathpal211-ux <somnathpal211@gmail.com>
2026-08-05 14:28:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let currentLanguage = "en";
 
 const productList = document.getElementById("product-list");
 const searchBox = document.getElementById("searchBox");
+const categoryFilter = document.getElementById("categoryFilter");
 const enBtn = document.getElementById("enBtn");
 const bnBtn = document.getElementById("bnBtn");
 
@@ -9,50 +10,74 @@ function showProducts(list) {
   productList.innerHTML = "";
 
   list.forEach(product => {
-    const name = currentLanguage === "bn"
-      ? product.name_bn
-      : product.name_en;
+
+    const name =
+      currentLanguage === "bn"
+        ? product.name_bn
+        : product.name_en;
 
     productList.innerHTML += `
       <div class="product-card">
         <h3>${name}</h3>
+
         <p>${product.unit}</p>
+
         <p>৳ ${product.price || "--"}</p>
-        <a class="order-btn"
-           target="_blank"
-           href="https://wa.me/918250266060?text=${encodeURIComponent("I want to order: " + name)}">
-           🟢 Order on WhatsApp
+
+        <a
+          class="order-btn"
+          target="_blank"
+          href="https://wa.me/918250266060?text=${encodeURIComponent("I want to order: " + name)}">
+          🟢 Order on WhatsApp
         </a>
+
       </div>
     `;
   });
 }
 
+function filterProducts() {
+
+  const keyword = searchBox.value.toLowerCase();
+
+  const category = categoryFilter.value;
+
+  const filtered = products.filter(product => {
+
+    const matchName =
+      product.name_en.toLowerCase().includes(keyword) ||
+      product.name_bn.includes(keyword);
+
+    const matchCategory =
+      category === "All Categories" ||
+      product.category === category;
+
+    return matchName && matchCategory;
+
+  });
+
+  showProducts(filtered);
+
+}
+
 showProducts(products);
 
-if (searchBox) {
-  searchBox.addEventListener("input", function () {
-    const keyword = this.value.toLowerCase();
+searchBox.addEventListener("input", filterProducts);
 
-    const filtered = products.filter(product =>
-      product.name_en.toLowerCase().includes(keyword) ||
-      product.name_bn.includes(keyword)
-    );
+categoryFilter.addEventListener("change", filterProducts);
 
-    showProducts(filtered);
-  });
-}
+enBtn.addEventListener("click", () => {
 
-if (enBtn) {
-  enBtn.addEventListener("click", function () {
-    currentLanguage = "en";
-    showProducts(products);
-  });
-}
+  currentLanguage = "en";
 
-if (bnBtn) {
-  bnBtn.addEventListener("click", function () {
-    currentLanguage = "bn";
-    showProducts(products);
-  });
-}
+  filterProducts();
+
+});
+
+bnBtn.addEventListener("click", () => {
+
+  currentLanguage = "bn";
+
+  filterProducts();
+
+});
