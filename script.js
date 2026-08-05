1c9c66ea7cfdfa81365e3ecdfcff982f82879eae
somnathpal211-ux <somnathpal211@gmail.com>
2026-08-05 08:48:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,53 +6,42 @@ const enBtn = document.getElementById("enBtn");
 const bnBtn = document.getElementById("bnBtn");
 
 function showProducts(list) {
-  if (!productList) return;
-
   productList.innerHTML = "";
 
   list.forEach(product => {
-    const name =
-      currentLanguage === "bn"
-        ? product.name_bn
-        : product.name_en;
+    const name = currentLanguage === "bn"
+      ? product.name_bn
+      : product.name_en;
 
-    const card = document.createElement("div");
-    card.className = "product-card";
-
-    card.innerHTML = `
-      <h3>${name}</h3>
-      <p>${product.unit}</p>
-      <p>৳ ${product.price || "--"}</p>
+    productList.innerHTML += `
+      <div class="product-card">
+        <h3>${name}</h3>
+        <p>${product.unit}</p>
+        <p>৳ ${product.price || "--"}</p>
+      </div>
     `;
-
-    productList.appendChild(card);
   });
 }
 
 showProducts(products);
-if (searchBox) {
-  searchBox.addEventListener("input", function () {
-    const keyword = this.value.toLowerCase();
 
-    const filtered = products.filter(product =>
-      product.name_en.toLowerCase().includes(keyword) ||
-      product.name_bn.toLowerCase().includes(keyword)
-    );
+searchBox.addEventListener("input", function () {
+  const keyword = this.value.toLowerCase();
 
-    showProducts(filtered);
-  });
-}
+  const filtered = products.filter(product =>
+    product.name_en.toLowerCase().includes(keyword) ||
+    product.name_bn.toLowerCase().includes(keyword)
+  );
 
-if (enBtn) {
-  enBtn.addEventListener("click", function () {
-    currentLanguage = "en";
-    showProducts(products);
-  });
-}
+  showProducts(filtered);
+});
 
-if (bnBtn) {
-  bnBtn.addEventListener("click", function () {
-    currentLanguage = "bn";
-    showProducts(products);
-  });
-}
+enBtn.addEventListener("click", function () {
+  currentLanguage = "en";
+  showProducts(products);
+});
+
+bnBtn.addEventListener("click", function () {
+  currentLanguage = "bn";
+  showProducts(products);
+});
